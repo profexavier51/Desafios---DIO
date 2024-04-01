232b5a9e91618a7005a43946adb435e4e4e1af0d
profexavier51 <profxavier51@outlook.com>
2024-04-01 10:18:54 -0300

    Subindo o arquivo para o Github

---
+++ desafioDIO.rb
@@ -0,0 +1,12 @@
+#solicitando ao usuario que digite seu nome
+print "Entre com seu Nome "
+nome=gets.chomp
+#solicitando ao usuario que digite seu sobrenome
+print "Entre com seu Sobrenome "
+sobrenome=gets.chomp
+#solicitando ao usuario que digite sua idade
+print "Entre com sua Idade Nome "
+idade=gets.chomp
+
+# exibindo toda a informação informada pelo usuário
+puts "O nome do usuario #{nome} #{sobrenome} tem #{idade} anos"
